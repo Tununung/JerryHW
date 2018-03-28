b2c950bb96174f39618e33528422bcf59d356104
Tununung <37055373+Tununung@users.noreply.github.com>
2018-03-29 00:37:51 +0800

    0326
    hand in

---
+++ 0326/source/calc.py
@@ -0,0 +1,11 @@
+def oper(a,b,c):
+    if c == "+":
+        return a+b
+    elif c == "-":
+        return a-b
+    elif c == "*":
+        return a*b
+    elif c == "/":
+        return a/b
+    else:
+        return"只能加減乘除喔"
